631ce3f104bdc3e2b62a07d19a90de02b60551b5
AyushDevHub <coder.ayushshaw@gmail.com>
2026-04-01 16:31:17 +0530

    Database is connected with MongoDB and Mongoose !

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,11 +1,15 @@
-import express from 'express';
+import app from './app.js';
+import dotenv from 'dotenv';
+import connectDB from './config/db.js';
 
-const app = express();
+// Connect to MongoDB
+dotenv.config();
+connectDB();
+
+
 const PORT = process.env.PORT || 3000;
+console.log("MongoDB Connected");
 
-app.get('/', (req, res) => {
-    res.send('This is my stock sim backend server! And it is running smoothly.');
-});
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
